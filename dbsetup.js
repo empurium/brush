7d659ec50bf0475a51f5335b5b5355e55317f844
Michael Simmons <github@e7i.org>
2013-11-19 01:23:39 -0800

    Removing mongo stuff (deprecating)

--- dbsetup.js
+++
@@ -1,7 +0,0 @@
-var mongoskin = require('mongoskin');
-var db        = mongoskin.db('localhost/brush?auto_reconnect');
-
-db.collection('events').ensureIndex(['name', 1]);
-db.collection('events').ensureIndex(['start', 1]);
-db.collection('events').ensureIndex(['end', 1]);
-db.collection('events').ensureIndex(['public', 1]);
--- mongotest.js
+++
@@ -1,7 +0,0 @@
-var mongoskin = require('mongoskin');
-var db        = mongoskin.db('localhost/brush?auto_reconnect');
-
-db.collection('events').insert({ sup: 'dude' });
-db.collection('events').find().toArray(function(err, events) {
-	console.log(events);
-});
