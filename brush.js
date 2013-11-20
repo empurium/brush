e74fffc38eaacd7517dd780281564d0e6dc8d2cd
Michael Simmons <github@e7i.org>
2013-11-20 11:23:11 -0800

    Cleanup and organization, output updates

--- brush.js
+++ brush.js
@@ -4,8 +4,8 @@ var fs        = require('fs');
 var ExifImage = require('exif').ExifImage;
 
 //var unsortedDir = '/space/Unsorted/Pictures/Picasa'; // no trailing slash
-var unsortedDir = '/Users/empurium/code/brush/pics';     // no trailing slash
-var archiveDir  = '/Users/empurium/code/brush/archive';  // no trailing slash
+var unsortedDir = '/home/empurium/code/brush/pics';     // no trailing slash
+var archiveDir  = '/home/empurium/code/brush/archive';  // no trailing slash
 var exifTypes   = /jpg/i;
 var slash       = '/';        // use '\\' on Windows
 
@@ -29,66 +29,64 @@ fs.readdir(unsortedDir, function(err, events) {
 function processEvent(eventDir, eventName) {
 	var eventStart            = new Date();
 	var eventEnd              = new Date();
-	var eventFiles            = [];
 	    eventRanges[eventDir] = [];
 
 	var files = fs.readdirSync(eventDir);
 
-	async.eachLimit(files, 1, function iter(fileName, next) {
-		var filePath = eventDir + slash + fileName;
+	async.eachLimit(files, 1,
+		function iter(fileName, next) {
+			var filePath = eventDir + slash + fileName;
 
-		if (fileName === '.picasa.ini') {
-			parsePicasaIni(filePath);
-			next();
-		} else {
-			getFileDate(filePath, function(fileDate) {
-				if (fileDate < eventStart) {
-					eventStart = fileDate;
-					eventEnd   = eventStart;
-				}
-				if (fileDate > eventEnd) {
-					eventEnd = fileDate;
-				}
-
-				console.log(eventName + ': ' + fileDate + ' -> ' + fileName);
-
-				eventRanges[eventDir]['start'] = eventStart;
-				eventRanges[eventDir]['end']   = eventEnd;
-
+			if (fileName === '.picasa.ini') {
+				parsePicasaIni(filePath);
 				next();
-			});
-		}
-	},
-	function done(err) {
-		var year  = eventStart.getFullYear();
-		var month = eventEnd.getMonth() * 1 + 1;
-		    month = (month < 10) ? '0' + month : month;
-		var newEventDir = archiveDir + slash + year + slash + month + slash + eventName;
-		console.log(eventName + ' -> ' + newEventDir);
+			} else {
+				getFileDate(filePath, function(fileDate) {
+					if (fileDate < eventStart) {
+						eventStart = fileDate;
+						eventEnd   = eventStart;
+					}
+					if (fileDate > eventEnd) {
+						eventEnd = fileDate;
+					}
 
-		// process each file to figure out this event
-		var files = fs.readdirSync(eventDir);
-		async.eachLimit(files, 1, function iter(fileName, next) {
+					eventRanges[eventDir]['start'] = eventStart;
+					eventRanges[eventDir]['end']   = eventEnd;
+
+					next();
+				});
+			}
+		},
+		function done(err) {
+			var year  = eventStart.getFullYear();
+			var month = eventEnd.getMonth() * 1 + 1;
+			    month = (month < 10) ? '0' + month : month;
+			var newEventDir = archiveDir + slash + year + slash + month + slash + eventName;
+
+			console.log(eventName + ' began: ' + eventStart);
+			console.log(eventName + ' ended: ' + eventEnd);
+
+			moveFiles(eventName, eventDir, newEventDir);
+		}
+	);
+}
+
+function moveFiles(eventName, eventDir, newEventDir) {
+	var files = fs.readdirSync(eventDir);
+
+	async.eachLimit(files, 1,
+		function iter(fileName, next) {
 			var filePath    = eventDir + slash + fileName;
 			var newFilePath = newEventDir + slash + fileName;
 
 			mkdirp(newEventDir, function(err) {
-				fs.stat(filePath, function(err, stat) {
-					//fs.rename(filePath, newFilePath, function(err) {
-						//fs.utimes(newFilePath, stat.atime, stat.mtime, function(err) {
-							//console.log(' - ' + filePath + ' -> ' + newFilePath);
-							eventFiles.push(newFilePath);
-							next();
-						//});
-					//});
-				});
+				//fs.rename(filePath, newFilePath, function(err) {
+					//console.log(' - ' + filePath + ' -> ' + newFilePath);
+					next();
+				//});
 			});
-		},
-		function done(err) {
-			console.log(eventName + ' began: ' + eventStart);
-			console.log(eventName + ' ended: ' + eventEnd);
-		});
-	});
+		}
+	);
 }
 
 function parseDate(dateString) {
@@ -110,15 +108,13 @@ function getFileDate(filePath, callback) {
 		new ExifImage({ image: filePath }, function(err, exif) {
 			//if (err) throw err;
 
-			console.log(filePath + ': ');
-			//console.log(exif);
 			//console.log('exif.image.ModifyDate: ' + exif.image.ModifyDate);
 			//console.log('exif.exif.CreateDate: ' + exif.exif.CreateDate);
 			//console.log('exif.exif.DateTimeOriginal: ' + exif.exif.DateTimeOriginal);
 
 			if (exif && exif.exif && exif.exif.DateTimeOriginal) {
 				fileDate = parseDate(exif.exif.DateTimeOriginal);
-				//console.log(fileDate);
+				console.log(filePath + ' EXIF DATE: ' + fileDate);
 
 				if (typeof callback === 'function') {
 					callback(fileDate);
@@ -136,6 +132,7 @@ function getFileDate(filePath, callback) {
 		if (fileDate === false) {
 			fileDate = stat.mtime;
 		}
+		console.log(filePath + ' DATE STAMP: ' + fileDate);
 
 		if (typeof callback === 'function') {
 			callback(fileDate);
